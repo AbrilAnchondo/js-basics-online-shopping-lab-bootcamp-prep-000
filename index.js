6d17618c870e09fe6302ce67a169379fdb7caa02
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-08-11 18:11:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -72,8 +72,9 @@ function removeFromCart(item) {
     return 'Sorry, we don\'t have a credit card on file for you.';
    
       } else {
-        cart = [];
+       
         var cost = 'Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.';
+         cart = [];
         return cost;
       } 
      
