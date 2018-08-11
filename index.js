a90e2bdc9a9bdaafb5e255c4feeec50bdc1719f1
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-08-11 17:56:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,6 +74,8 @@ function removeFromCart(item) {
       } else {
        return 'Your total cost is $' + total() + ', which will be charged to the card ' + cardNumber + '.';
       } 
+      setCart (0);
+      
   }
      
  
